345277a2f7103ec544b14ec1a96cd7427a8c27f5
Flavio Moreira <flaviorsm@gmail.com>
2022-04-02 22:07:15 -0300

    Ajuste deploy.

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://staging-medicarsaude-api.herokuapp.com/api',
+  apiUrl: 'https://staging-medicarsaude-api.herokuapp.com/api',
   sessionStorage: 'medicarAuth',
 };
